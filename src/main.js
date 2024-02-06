8b85fb19f50e8cd686ac5054d06f74f58e473d5c
Mohammed Taqi <taqealibade@gmail.com>
2024-02-06 16:06:09 +0300

    adding vue-router

--- src/main.js
+++ src/main.js
@@ -4,7 +4,24 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+// import router from './router'
+import * as VueRouter from 'vue-router'
+
+import PageHomeVue from './components/PageHome.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: PageHomeVue
+  }
+]
+
+const router = VueRouter.createRouter(
+  {
+    history: VueRouter.createWebHashHistory(),
+    routes
+  }
+)
 
 const app = createApp(App)
 
