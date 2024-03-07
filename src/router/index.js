388f7fd5f573fbac7355753281dbfb51010ef76a
Mohammed Taqi <taqealibade@gmail.com>
2024-03-07 16:48:54 +0300

    Redirect With Vue Router Without Changing the URL

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PageHomeVue from '@/components/PageHome.vue'
 import PageThreadShow from '@/components/PageThreadShow.vue'
 import NotFound from '@/components/NotFound.vue'
-
+import sourceData from '@/data.json'
 const routes = [
   {
     path: '/',
@@ -13,7 +13,25 @@ const routes = [
     path: '/thread/:id',
     name: 'ThreadShow',
     component: PageThreadShow,
-    props: true
+    props: true,
+    beforeEnter (to, from, next) {
+      // check if thread exits
+      const threadExist = sourceData.threads.find((thread) => thread.id === to.params.id)
+      // if exit continue
+      if (threadExist) {
+        return next()
+      } else {
+        // if doesnt exit redirect to not found
+        next({
+          name: 'NotFound',
+          // preserve current path and remove the first char to avoid the target URL starting with `//`
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve existing query and hash if any
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
   },
   {
     path: '/:pathMatch(.*)*',
