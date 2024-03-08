c7d63d81d723ad06107122ffc7412f75702ba3bf
Mohammed Taqi <taqealibade@gmail.com>
2024-03-09 00:16:03 +0300

    Creating a ForumList Component

---
+++ src/components/ForumList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="col-full">
+    <div class="forum-list">
+      <h2 class="list-title">
+        <a href="#">Forums</a>
+      </h2>
+
+      <div class="forum-listing" v-for="forum in props.forums" :key="forum.id">
+        <div class="forum-details">
+          <router-link :to="{ name: 'Forum', params: { id: forum.id } }" class="forum-name">
+            {{ forum.name }}
+          </router-link>
+          <p class="forum-description">{{ forum.description }}</p>
+        </div>
+
+        <div class="threads-count">
+          <p>
+            <span class="count text-lead">{{ forum.threads?.length }}</span>
+          </p>
+          {{ forumThreadsWord(forum) }}
+        </div>
+
+        <div class="last-thread"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  forums: {
+    require: true,
+    type: Array
+  }
+})
+
+// Methods
+const forumThreadsWord = (forum) => {
+  if (forum.threads?.length) {
+    return forum.threads.length > 1 ? 'threads' : 'thread'
+  } else {
+    return 'no threads'
+  }
+}
+</script>
--- src/views/PageHome.vue
+++ src/views/PageHome.vue
@@ -1,9 +1,10 @@
 <script setup>
 import sourceData from '@/data.json'
-import ThreadList from '@/components/ThreadList.vue'
+import ForumList from '@/components/ForumList.vue'
 import { reactive } from 'vue'
 
-const threads = reactive(sourceData.threads)
+// const threads = reactive(sourceData.threads)
+const forums = reactive(sourceData.forums)
 
 // function postById (postId) {
 //   return posts.find((p) => p.id === postId)
@@ -11,10 +12,9 @@ const threads = reactive(sourceData.threads)
 // function userById (userId) {
 //   return users.find((p) => p.id === userId)
 // }
-
 </script>
 
 <template>
   <h1>Welcome to the Forum</h1>
-  <ThreadList :threads="threads"/>
+  <ForumList :forums="forums" />
 </template>
