c81033e094fc1c216024f4fc348202d68535210e
Mohammed Taqi <taqealibade@gmail.com>
2024-03-08 16:10:58 +0300

    Communication Between Components

--- src/views/PageThreadShow.vue
+++ src/views/PageThreadShow.vue
@@ -2,41 +2,15 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <PostList :posts="threadPosts" />
-
-    <div class="col-full">
-      <form @submit.prevent="addPost">
-        <div class="form-group">
-          <label for="thread_title">Title:</label>
-          <input type="text" id="thread_title" class="form-input" name="title" />
-        </div>
-
-        <div class="form-group">
-          <label for="thread_content">Content:</label>
-          <textarea
-            v-model="newPostText"
-            id="thread_content"
-            class="form-input"
-            name="content"
-            rows="8"
-            cols="140"
-          ></textarea>
-        </div>
-
-        <div class="btn-group">
-          <button class="btn btn-ghost">Cancel</button>
-          <button class="btn btn-blue" type="submit" name="Publish">Publish</button>
-        </div>
-      </form>
-    </div>
+    <PostEditor @save="addPost"/>
   </div>
 </template>
 
 <script setup>
+import PostEditor from '@/components/PostEditor.vue'
 import PostList from '@/components/PostList.vue'
 import sourceData from '@/data.json'
-import { reactive, computed, ref } from 'vue'
-
-const newPostText = ref('')
+import { reactive, computed } from 'vue'
 
 const threads = reactive(sourceData.threads)
 const posts = reactive(sourceData.posts)
@@ -49,7 +23,6 @@ const props = defineProps({
 })
 
 const thread = computed(() => {
-  // eslint-disable-next-line no-undef
   return threads.find((thread) => thread.id === props.id)
 })
 
@@ -58,31 +31,16 @@ const threadPosts = computed(() => {
 })
 
 // Methods
-// const addPost = () => {
-//   const postId = 'gggg' + Math.random()
-//   const post = {
-//     id: postId,
-//     text: newPostText,
-//     publishedAt: Math.floor(Date.now() / 1000),
-//     threadId: thread.value.id,
-//     userId: 'ALXhxjwgY9PinwNGHpfai6OWyDu2'
-//   }
-//   posts.push(post)
-//   thread.value.post.push(postId)
-//   newPostText.value = ''
-// }
-
-function addPost () {
+const addPost = (eventData) => {
+  console.log(eventData)
   const postId = 'gggg' + Math.random()
   const post = {
-    id: postId,
-    text: newPostText,
-    publishedAt: Math.floor(Date.now() / 1000),
-    threadId: thread.value.id,
-    userId: 'ALXhxjwgY9PinwNGHpfai6OWyDu2'
+    ...eventData.post,
+    threadId: thread.value.id
   }
-  this.posts.push(post)
-  this.thread.posts.push(postId)
-  newPostText.value = ''
+  console.log(eventData.post)
+
+  posts.push(post)
+  thread.value.posts.push(postId)
 }
 </script>
