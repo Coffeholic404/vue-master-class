cd3eb8e1c6a030a0fab0c0fcc8b706ecd23d3697
Mohammed Taqi <taqealibade@gmail.com>
2024-03-11 03:11:01 +0300

    ctreating the userProfileCard component

---
+++ src/components/UserProfileCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="profile-card">
+    <p class="text-center">
+      <img
+        :src="userStore.authUser.avatar"
+        :alt="`${userStore.authUser.name} profile picture`"
+        class="avatar-xlarge"
+      />
+    </p>
+
+    <h1 class="title">{{ userStore.authUser.username }}</h1>
+
+    <p class="text-lead">{{ userStore.authUser.name }}</p>
+
+    <p class="text-justify">{{ userStore.authUser.bio || 'No bio specified.' }}</p>
+
+    <span class="online">{{ userStore.authUser.username }} is online</span>
+
+    <div class="stats">
+      <span>{{ userPostsCount }} posts</span>
+      <span>{{ userThreadsCount }} threads</span>
+    </div>
+
+    <hr />
+
+    <p v-if="userStore.authUser.wesite" class="text-large text-center">
+      <i class="fa fa-globe"></i>
+      <a :href="userStore.authUser.website">{{ userStore.authUser.wesite }}</a>
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from '@/stores/UserStore'
+import { computed } from 'vue'
+const userStore = useUserStore()
+const userPostsCount = computed(() => userStore.userPosts.length)
+const userThreadsCount = computed(() => userStore.userThreads.length)
+// const props = defineProps({
+
+// })
+</script>
--- src/views/PageProfile.vue
+++ src/views/PageProfile.vue
@@ -2,36 +2,7 @@
   <div class="container">
     <div class="flex-grid">
       <div class="col-3 push-top">
-        <div class="profile-card">
-          <p class="text-center">
-            <img
-              :src="userStore.authUser.avatar"
-              :alt="`${userStore.authUser.name} profile picture`"
-              class="avatar-xlarge"
-            />
-          </p>
-
-          <h1 class="title">{{ userStore.authUser.username }}</h1>
-
-          <p class="text-lead">{{ userStore.authUser.name }}</p>
-
-          <p class="text-justify">{{ userStore.authUser.bio || 'No bio specified.' }}</p>
-
-          <span class="online">{{ userStore.authUser.username }} is online</span>
-
-          <div class="stats">
-            <span>{{ userPostsCount }} posts</span>
-            <span>{{ userThreadsCount }} threads</span>
-          </div>
-
-          <hr />
-
-          <p v-if="userStore.authUser.wesite" class="text-large text-center">
-            <i class="fa fa-globe"></i>
-            <a :href="userStore.authUser.website">{{ userStore.authUser.wesite }}</a>
-          </p>
-        </div>
-
+        <UserProfileCard />
         <p class="text-xsmall text-faded text-center">
           Member since june 2003, last visited 4 hours ago
         </p>
@@ -57,13 +28,7 @@
 
 <script setup>
 import PostList from '@/components/PostList.vue'
+import UserProfileCard from '@/components/UserProfileCard.vue'
 import { useUserStore } from '@/stores/UserStore'
-import { computed } from 'vue'
 const userStore = useUserStore()
-console.log(userStore.userPosts.value)
-
-const userPostsCount = computed(() => userStore.userPosts.length)
-const userThreadsCount = computed(() => userStore.userThreads.length)
-
-console.log(userPostsCount.value, userThreadsCount.value)
 </script>
